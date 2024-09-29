accd276dbe9ff17360d90cf7a9f20c092b6b4e5f
Roman Romanov <rromanov.vk@gmail.com>
2024-09-29 22:59:16 +0500

    Initial Commit

---
+++ MoexCalculator/MoexCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoexCalculatorApp.swift
+//  MoexCalculator
+//
+//  Created by Roman Romanov on 29.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoexCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
